5d2304732399fd41683ab8938a6f75a7454fe71e
bingo.chen <finesoft@gmail.com>
2019-01-06 14:53:21 +0800

    No message

--- corant-asosat/corant-asosat-ddd/src/test/java/org/corant/CorantJunit4ClassRunnerTest1.java
+++ corant-asosat/corant-asosat-ddd/src/test/java/org/corant/CorantJunit4ClassRunnerTest1.java
@@ -16,22 +16,14 @@
 
 package org.corant;
 
-import static org.corant.shared.util.MapUtils.getMapString;
-import java.sql.SQLException;
 import java.util.concurrent.CountDownLatch;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Inject;
-import javax.inject.Named;
 import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
 import javax.transaction.Transactional;
-import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.corant.devops.test.unit.CorantJUnit4ClassRunner;
-import org.corant.shared.normal.Names.JndiNames;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -47,37 +39,10 @@ import org.junit.runner.RunWith;
 public class CorantJunit4ClassRunnerTest1 {
 
   @Inject
-  @Named("dmmsRwDs")
-  DataSource ds;
-
-  @Inject
   InitialContext jndi;
 
   @Inject
   Logger logger;
-
-  @Test
-  public void testDataSource() throws SQLException {
-    logger.info("test datasource " + ds);
-    new QueryRunner(ds).query("SELECT * FROM CT_DMMS_INDU", new MapListHandler()).forEach(m -> {
-      System.out.println(getMapString(m, "name"));
-    });
-  }
-
-  @Test
-  public void testDataSourceJndi() throws NamingException, SQLException {
-    ds.getConnection();
-    logger.info("test jndi datasource " + jndi.lookup(JndiNames.JNDI_DATS_NME + "/dmmsRwDs"));
-  }
-
-  @Test
-  @Transactional
-  public void testDataSourceUpdater() throws SQLException {
-    String name = "汽车零部件制造业";
-    System.out.println(new QueryRunner(ds).execute("UPDATE CT_DMMS_INDU SET NAME = ? WHERE id = ?",
-        name, 379005035389915137l));
-    // throw new CorantRuntimeException("==FALURE");
-  }
 
   @Test
   public void testMultiThreads() throws InterruptedException {

1acf92955399dddd87996c16a4876d9006854cf6
bingo.chen <finesoft@gmail.com>
2019-01-08 15:18:36 +0800

    No message

--- corant-devops/corant-devops-maven/corant-devops-maven-jandex/src/main/java/org/corant/devops/maven/jandex/Jandexer.java
+++ corant-devops/corant-devops-maven/corant-devops-maven-jandex/src/main/java/org/corant/devops/maven/jandex/Jandexer.java
@@ -1,14 +1,16 @@
 /*
  * Copyright (c) 2013-2018, Bingo.Chen (finesoft@gmail.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
  * the License.
  */
 package org.corant.devops.maven.jandex;
@@ -47,6 +49,10 @@ public class Jandexer extends AbstractMojo {
       return;
     }
     final File clsDir = new File(project.getBuild().getOutputDirectory());
+    if (!clsDir.exists()) {
+      getLog().warn("(corant) can not find file dir, the directory is " + clsDir.getPath());
+      return;
+    }
     final Indexer indexer = new Indexer();
     final DirectoryScanner scanner = new DirectoryScanner();
     getLog().debug(

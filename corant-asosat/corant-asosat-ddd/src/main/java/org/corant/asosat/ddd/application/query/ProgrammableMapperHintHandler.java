e9ad032d9a8ac51de7a370e3fa13191089ebd266
bingo.chen <finesoft@gmail.com>
2019-02-21 20:32:41 +0800

    No message

--- corant-asosat/corant-asosat-ddd/src/main/java/org/corant/asosat/ddd/application/query/ProgrammableMapperHintHandler.java
+++ corant-asosat/corant-asosat-ddd/src/main/java/org/corant/asosat/ddd/application/query/ProgrammableMapperHintHandler.java
@@ -13,10 +13,24 @@
  */
 package org.corant.asosat.ddd.application.query;
 
+import static org.corant.shared.util.Empties.isEmpty;
+import static org.corant.shared.util.ObjectUtils.isEquals;
+import static org.corant.shared.util.StringUtils.isNotBlank;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
+import javax.script.Bindings;
+import javax.script.CompiledScript;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import org.corant.shared.exception.CorantRuntimeException;
 import org.corant.suites.ddd.annotation.stereotype.ApplicationServices;
 import org.corant.suites.query.mapping.QueryHint;
 import org.corant.suites.query.spi.ResultHintHandler;
+import jdk.nashorn.api.scripting.NashornScriptEngine;
 
 /**
  * corant-asosat-ddd
@@ -29,12 +43,59 @@ import org.corant.suites.query.spi.ResultHintHandler;
 public class ProgrammableMapperHintHandler implements ResultHintHandler {
 
   public static final String HINT_NAME = "result-mapper";
-  public static final String HNIT_SCRIPT = "script-lang";
-  public static final String HNIT_SCRIPT_FUNC_PARAM = "script-param";
+  public static final String HNIT_SCRIPT_ENGINE = "script-engine";
+  public static final String DFLT_SCRIPT_ENGINE = "nashorn";
+
+  final ScriptEngineManager engineManager = new ScriptEngineManager();
+  // ScriptEngine and CompiledScript are not thread safe, we use state-less so it thread safe.
+  final Map<String, ScriptEngine> engines = new ConcurrentHashMap<>();
+  final Map<String, CompiledScript> scripts = new ConcurrentHashMap<>();
+
+  @Override
+  public boolean canHandle(QueryHint hint) {
+    return hint != null && isEquals(hint.getKey(), HINT_NAME) && isNotBlank(hint.getScript());
+  }
 
   @Override
   public void handle(QueryHint qh, Object result) {
-
+    final ScriptEngine se = resolveEngine(qh);
+    final CompiledScript cs = resolveScript(qh, se);
+    if (cs != null) {
+      Bindings bindings = se.createBindings();
+      try {
+        cs.eval(bindings);
+      } catch (ScriptException e) {
+      }
+    }
   }
 
+  protected ScriptEngine resolveEngine(QueryHint qh) {
+    String name = DFLT_SCRIPT_ENGINE;
+    if (!isEmpty(qh.getParameters(HNIT_SCRIPT_ENGINE))) {
+      if (isNotBlank(qh.getParameters(HNIT_SCRIPT_ENGINE).get(0).getValue())) {
+        name = qh.getParameters(HNIT_SCRIPT_ENGINE).get(0).getValue();
+      }
+    }
+    return engines.computeIfAbsent(name, (en) -> engineManager.getEngineByName(en));
+  }
+
+  protected CompiledScript resolveScript(QueryHint qh, ScriptEngine engine) {
+    String script = qh.getScript();
+    return scripts.computeIfAbsent(script, (s) -> {
+      try {
+        if (engine instanceof NashornScriptEngine) {
+          return NashornScriptEngine.class.cast(engine).compile(s);
+        }
+        return null;
+      } catch (ScriptException e) {
+        throw new CorantRuntimeException(e);
+      }
+    });
+  }
+
+  @PostConstruct
+  void onPostConstruct() {}
+
+  @PreDestroy
+  void onPreDestroy() {}
 }

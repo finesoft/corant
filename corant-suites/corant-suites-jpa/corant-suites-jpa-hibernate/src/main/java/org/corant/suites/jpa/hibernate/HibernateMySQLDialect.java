0b91eceabacd85aa9c65fd2ed8896daa6d2f1c5b
bingo.chen <finesoft@gmail.com>
2019-03-22 15:46:21 +0800

    No message

--- corant-suites/corant-suites-jpa/corant-suites-jpa-hibernate/src/main/java/org/corant/suites/jpa/hibernate/HibernateMySQLDialect.java
+++ corant-suites/corant-suites-jpa/corant-suites-jpa-hibernate/src/main/java/org/corant/suites/jpa/hibernate/HibernateMySQLDialect.java
@@ -1,16 +1,14 @@
 /*
  * Copyright (c) 2013-2018, Bingo.Chen (finesoft@gmail.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package org.corant.suites.jpa.hibernate;
@@ -27,6 +25,7 @@ import org.hibernate.type.StandardBasicTypes;
  *
  */
 public class HibernateMySQLDialect extends MySQL57Dialect {
+
   private static final int MAX_LENGTH = 4000;
 
   public HibernateMySQLDialect() {
@@ -35,6 +34,7 @@ public class HibernateMySQLDialect extends MySQL57Dialect {
     this.registerColumnType(Types.CLOB, "longtext");
     this.registerColumnType(Types.NCLOB, "longtext");
     this.registerColumnType(Types.LONGNVARCHAR, "longtext");
+    this.registerColumnType(Types.NUMERIC, "numeric(19,6)");
     this.registerHibernateType(Types.NVARCHAR, StandardBasicTypes.STRING.getName());
     this.registerHibernateType(Types.NCLOB, StandardBasicTypes.STRING.getName());
     this.registerHibernateType(Types.LONGNVARCHAR, StandardBasicTypes.STRING.getName());

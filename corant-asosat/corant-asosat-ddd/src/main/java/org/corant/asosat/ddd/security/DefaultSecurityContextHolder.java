449c3f8e36eda5395d5132d08d3e2fb198c5c194
bingo.chen <finesoft@gmail.com>
2019-04-22 19:24:47 +0800

    No message

--- corant-asosat/corant-asosat-ddd/src/main/java/org/corant/asosat/ddd/security/DefaultSecurityContextHolder.java
+++ corant-asosat/corant-asosat-ddd/src/main/java/org/corant/asosat/ddd/security/DefaultSecurityContextHolder.java
@@ -13,11 +13,18 @@
  */
 package org.corant.asosat.ddd.security;
 
+import static org.corant.shared.util.ObjectUtils.defaultObject;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.ws.rs.core.SecurityContext;
 import org.corant.asosat.ddd.domain.shared.Participator;
 import org.corant.suites.ddd.annotation.stereotype.InfrastructureServices;
 import org.corant.suites.security.shared.SecurityContextHolder;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 /**
  * corant-asosat-ddd
@@ -31,14 +38,58 @@ public class DefaultSecurityContextHolder implements SecurityContextHolder {
 
   static final ThreadLocal<DefaultSecurityContext> holder = new ThreadLocal<>();
 
+  @Inject
+  @ConfigProperty(name = "security.enable-default-context", defaultValue = "false")
+  Boolean enableDefaultContext;
+
+  @Inject
+  @ConfigProperty(name = "security.default-context-org-id")
+  Optional<String> defaultOrgId;
+
+  @Inject
+  @ConfigProperty(name = "security.default-context-org-name")
+  Optional<String> defaultOrgName;
+
+  @Inject
+  @ConfigProperty(name = "security.default-context-user-id")
+  Optional<String> defaultUserId;
+
+  @Inject
+  @ConfigProperty(name = "security.default-context-user-name")
+  Optional<String> defaultUserName;
+
+  @Inject
+  @ConfigProperty(name = "security.default-access-token")
+  Optional<String> defaultAccessToken;
+
+  @Inject
+  @ConfigProperty(name = "security.default-refresh-token")
+  Optional<String> defaultRefreshToken;
+
+  @Inject
+  @ConfigProperty(name = "security.default-authentication-scheme")
+  Optional<String> defaultAuthenticationScheme;
+
+  @Inject
+  @ConfigProperty(name = "security.default-secure", defaultValue = "false")
+  Boolean defaultSecure;
+
+  @Inject
+  @ConfigProperty(name = "security.default-user-roles", defaultValue = "false")
+  Optional<Set<String>> defaultUserRoles;
+
+  volatile DefaultSecurityContext defaultContxt;
+
   @Override
   public DefaultSecurityContext get() {
-    return holder.get();
+    return enableDefaultContext ? defaultObject(holder.get(), defaultContxt) : holder.get();
   }
 
   public Participator getCurrentOrg() {
     if (holder.get() != null) {
       return holder.get().getOrgPrincipal();
+    } else if (enableDefaultContext && defaultContxt != null) {
+      return defaultContxt.getOrgPrincipal();
     }
     return Participator.empty();
   }
@@ -46,6 +97,8 @@ public class DefaultSecurityContextHolder implements SecurityContextHolder {
   public Participator getCurrentUser() {
     if (holder.get() != null) {
       return holder.get().getUserPrincipal();
+    } else if (enableDefaultContext && defaultContxt != null) {
+      return defaultContxt.getUserPrincipal();
     }
     return Participator.empty();
   }
@@ -65,4 +118,15 @@ public class DefaultSecurityContextHolder implements SecurityContextHolder {
     holder.remove();
   }
 
+  @PostConstruct
+  void onPostConstruct() {
+    if (enableDefaultContext) {
+      defaultContxt = new DefaultSecurityContext(defaultAccessToken.orElse(null),
+          defaultRefreshToken.orElse(null),
+          new Participator(defaultUserId.orElse(null), defaultUserName.orElse(null)),
+          new Participator(defaultOrgId.orElse(null), defaultOrgName.orElse(null)), defaultSecure,
+          defaultAuthenticationScheme.orElse(null),
+          defaultUserRoles.orElse(Collections.emptySet()));
+    }
+  }
 }

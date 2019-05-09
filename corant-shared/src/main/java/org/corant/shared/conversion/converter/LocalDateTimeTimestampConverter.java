a47078959816394846feaa23e41850b5dd5417d4
bingo.chen <finesoft@gmail.com>
2019-05-09 20:29:32 +0800

    No message

---
+++ corant-shared/src/main/java/org/corant/shared/conversion/converter/LocalDateTimeTimestampConverter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2013-2018, Bingo.Chen (finesoft@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.corant.shared.conversion.converter;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.Map;
+
+/**
+ * corant-shared
+ *
+ * @author bingo 上午11:09:37
+ *
+ */
+public class LocalDateTimeTimestampConverter extends AbstractConverter<LocalDateTime, Timestamp> {
+
+  public LocalDateTimeTimestampConverter() {
+    super();
+  }
+
+  /**
+   * @param throwException
+   */
+  public LocalDateTimeTimestampConverter(boolean throwException) {
+    super(throwException);
+  }
+
+  /**
+   * @param defaultValue
+   */
+  public LocalDateTimeTimestampConverter(Timestamp defaultValue) {
+    super(defaultValue);
+  }
+
+  /**
+   * @param defaultValue
+   * @param throwException
+   */
+  public LocalDateTimeTimestampConverter(Timestamp defaultValue, boolean throwException) {
+    super(defaultValue, throwException);
+  }
+
+  @Override
+  public boolean isPossibleDistortion() {
+    return false;
+  }
+
+  @Override
+  protected Timestamp convert(LocalDateTime value, Map<String, ?> hints) throws Exception {
+    return Timestamp.valueOf(value);
+  }
+
+}
---
+++ corant-shared/src/main/java/org/corant/shared/conversion/converter/SqlDateLocalDateConverter.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2013-2018, Bingo.Chen (finesoft@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.corant.shared.conversion.converter;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Map;
+
+/**
+ * corant-shared
+ *
+ * @author bingo 上午11:09:37
+ *
+ */
+public class SqlDateLocalDateConverter extends AbstractConverter<Date, LocalDate> {
+
+  public SqlDateLocalDateConverter() {
+    super();
+  }
+
+  /**
+   * @param throwException
+   */
+  public SqlDateLocalDateConverter(boolean throwException) {
+    super(throwException);
+  }
+
+  /**
+   * @param defaultValue
+   */
+  public SqlDateLocalDateConverter(LocalDate defaultValue) {
+    super(defaultValue);
+  }
+
+  /**
+   * @param defaultValue
+   * @param throwException
+   */
+  public SqlDateLocalDateConverter(LocalDate defaultValue, boolean throwException) {
+    super(defaultValue, throwException);
+  }
+
+  @Override
+  public boolean isPossibleDistortion() {
+    return false;
+  }
+
+  @Override
+  protected LocalDate convert(Date value, Map<String, ?> hints) throws Exception {
+    return value.toLocalDate();
+  }
+
+}

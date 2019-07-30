15e6868eafc44e90e0ade3b0de0c4514acfaa2eb
don <don@gixon.com>
2019-07-30 19:00:09 +0800

    rest easy cors filter

--- corant-suites/corant-suites-jaxrs/corant-suites-jaxrs-shared/src/main/java/org/corant/suites/jaxrs/shared/AbstractJaxrsCorsFilter.java
+++ corant-suites/corant-suites-jaxrs/corant-suites-jaxrs-shared/src/main/java/org/corant/suites/jaxrs/shared/AbstractJaxrsCorsFilter.java
@@ -18,38 +18,35 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
+
 import org.corant.suites.servlet.abstraction.AbstractCorsFilter;
 
 /**
  * corant-suites-jaxrs-shared
- *
  * @author bingo 下午2:51:41
- *
  */
 // @Provider
 // @PreMatching
 // @Priority(Priorities.AUTHORIZATION)
 // @ApplicationScoped
 public abstract class AbstractJaxrsCorsFilter extends AbstractCorsFilter
-    implements ContainerRequestFilter, ContainerResponseFilter {
+        implements ContainerRequestFilter, ContainerResponseFilter {
 
-  // TODO FIXME
-  @Override
-  public void filter(ContainerRequestContext requestContext) throws IOException {
-    if (isEnabled()) {
-      requestContext.getHeaders().add("Access-Control-Allow-Origin", getOrigin());
-      requestContext.getHeaders().add("Access-Control-Allow-Headers", getHeaders());
-      requestContext.getHeaders().add("Access-Control-Allow-Credentials", getCredentials());
-      requestContext.getHeaders().add("Access-Control-Allow-Methods", getMethods());
-      requestContext.getHeaders().add("Access-Control-Max-Age", getMaxAge());
+    // TODO FIXME
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
     }
-  }
 
-  // TODO FIXME
-  @Override
-  public void filter(ContainerRequestContext requestContext,
-      ContainerResponseContext responseContext) throws IOException {
-
-  }
-
+    // TODO FIXME
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        if (isEnabled()) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", getOrigin());
+            responseContext.getHeaders().add("Access-Control-Allow-Headers", getHeaders());
+            responseContext.getHeaders().add("Access-Control-Allow-Credentials", getCredentials());
+            responseContext.getHeaders().add("Access-Control-Allow-Methods", getMethods());
+            responseContext.getHeaders().add("Access-Control-Max-Age", getMaxAge());
+        }
+    }
 }

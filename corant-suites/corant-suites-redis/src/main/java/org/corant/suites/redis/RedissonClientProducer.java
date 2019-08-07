f173c36007f4a477febfc671f28a78ab1ed6888d
don <don43xu@qq.com>
2019-08-07 14:52:51 +0800

    RedissonClientProducer @Dependent; 删除其它无用类

--- corant-suites/corant-suites-redis/src/main/java/org/corant/suites/redis/RedissonClientProducer.java
+++ corant-suites/corant-suites-redis/src/main/java/org/corant/suites/redis/RedissonClientProducer.java
@@ -1,48 +1,52 @@
 package org.corant.suites.redis;
 
 import java.util.Optional;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.config.SingleServerConfig;
 
-@ApplicationScoped
+@Dependent
 public class RedissonClientProducer {
 
-  @Inject
-  @ConfigProperty(name = "redis.database")
-  private Optional<Integer> database;
+    @Inject
+    @ConfigProperty(name = "redis.database")
+    private Optional<Integer> database;
 
-  @Inject
-  @ConfigProperty(name = "redis.address")
-  private String address;
+    @Inject
+    @ConfigProperty(name = "redis.address")
+    private String address;
 
-  @Inject
-  @ConfigProperty(name = "redis.password")
-  private Optional<String> password;
+    @Inject
+    @ConfigProperty(name = "redis.password")
+    private Optional<String> password;
 
-  @Inject
-  @ConfigProperty(name = "redis.timeout")
-  private Optional<Integer> timeout;
+    @Inject
+    @ConfigProperty(name = "redis.timeout")
+    private Optional<Integer> timeout;
 
-  @Produces
-  @ApplicationScoped
-  public RedissonClient produce() {
-    Config config = new Config();
-    SingleServerConfig serverConfig = config.useSingleServer();
-    serverConfig.setAddress("redis://" + address);
-    password.ifPresent(serverConfig::setPassword);
-    database.ifPresent(serverConfig::setDatabase);
-    timeout.ifPresent(serverConfig::setTimeout);
-    return Redisson.create(config);
-  }
+    @Produces
+    @ApplicationScoped
+    public RedissonClient produce() {
+        Config config = new Config();
+        SingleServerConfig serverConfig = config.useSingleServer();
+        serverConfig.setAddress("redis://" + address);
+        password.ifPresent(serverConfig::setPassword);
+        database.ifPresent(serverConfig::setDatabase);
+        timeout.ifPresent(serverConfig::setTimeout);
+        return Redisson.create(config);
+    }
 
-  void dispose(@Disposes RedissonClient client) {
-    client.shutdown();
-  }
+    void dispose(@Disposes RedissonClient client) {
+        client.shutdown();
+    }
 }

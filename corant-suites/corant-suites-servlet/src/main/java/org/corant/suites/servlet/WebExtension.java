c6fd779572bcb7fe074313b8de31cd0190d93e84
bingo.chen <finesoft@gmail.com>
2020-01-07 14:01:08 +0800

    No message

--- corant-suites/corant-suites-servlet/src/main/java/org/corant/suites/servlet/WebExtension.java
+++ corant-suites/corant-suites-servlet/src/main/java/org/corant/suites/servlet/WebExtension.java
@@ -1,16 +1,14 @@
 /*
  * Copyright (c) 2013-2018, Bingo.Chen (finesoft@gmail.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package org.corant.suites.servlet;
@@ -19,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
 import javax.enterprise.inject.spi.ProcessAnnotatedType;
 import javax.enterprise.inject.spi.WithAnnotations;
@@ -89,6 +88,11 @@ public class WebExtension implements Extension, WebMetaDataProvider {
     servletMetaDatas
         .add(new WebServletMetaData(pat.getAnnotatedType().getAnnotation(WebServlet.class),
             servletSecurity, multipartConfig, pat.getAnnotatedType().getJavaClass()));
+  }
+
+  // Jandex
+  void onBeforeBeanDiscovery(@Observes BeforeBeanDiscovery bbd) {
+    // TODO use jandex to find out web meta data
   }
 
 }

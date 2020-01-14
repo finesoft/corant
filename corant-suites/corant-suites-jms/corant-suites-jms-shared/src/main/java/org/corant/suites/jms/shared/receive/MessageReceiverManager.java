ccbb9216d8f2b79cda4ad47f1ba4f62940a00cb8
bingo.chen <finesoft@gmail.com>
2020-01-14 15:06:56 +0800

    No message

--- corant-suites/corant-suites-jms/corant-suites-jms-shared/src/main/java/org/corant/suites/jms/shared/receive/MessageReceiverManager.java
+++ corant-suites/corant-suites-jms/corant-suites-jms-shared/src/main/java/org/corant/suites/jms/shared/receive/MessageReceiverManager.java
@@ -17,6 +17,7 @@ import static org.corant.shared.util.Assertions.shouldBeTrue;
 import static org.corant.shared.util.Assertions.shouldNotNull;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
@@ -30,6 +31,7 @@ import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.BeforeShutdown;
 import javax.inject.Inject;
 import org.corant.kernel.event.PostCorantReadyEvent;
+import org.corant.shared.util.ObjectUtils.Pair;
 import org.corant.suites.jms.shared.AbstractJMSConfig;
 import org.corant.suites.jms.shared.AbstractJMSExtension;
 
@@ -58,6 +60,7 @@ public class MessageReceiverManager {
   }
 
   void beforeShutdown(@Observes final BeforeShutdown e) {
+    logger.info(() -> "Shut down the message receiver executor services");
     executorServices.values().forEach(es -> es.shutdownNow().forEach(r -> {
       if (r instanceof MessageReceiverTask) {
         ((MessageReceiverTask) r).release(true);
@@ -66,23 +69,35 @@ public class MessageReceiverManager {
   }
 
   void onPostCorantReadyEvent(@Observes PostCorantReadyEvent adv) {
+    Set<Pair<String, String>> anycasts = new HashSet<>();
     for (final MessageReceiverMetaData metaData : receiveMetaDatas) {
+      if (!metaData.isMulticast()
+          && !anycasts.add(Pair.of(metaData.getConnectionFactoryId(), metaData.getDestination()))) {
+        logger.warning(() -> String.format(
+            "The anycast message receiver with same connection factory id and same destination appeared more than once, it should be avoided in general. message receiver [%s].",
+            metaData));
+      }
       final AbstractJMSConfig cfg =
           AbstractJMSExtension.getConfig(metaData.getConnectionFactoryId());
       if (cfg != null && cfg.isEnable()) {
         if (metaData.xa()) {
           shouldBeTrue(cfg.isXa(),
-              "Can not schedule xa message receiver, the connection factory [%s] not supported! message receiver [%s].",
+              "Can not schedule xa message receiver task, the connection factory [%s] not supported! message receiver [%s].",
               cfg.getConnectionFactoryId(), metaData);
         }
         ScheduledExecutorService ses = shouldNotNull(
             executorServices.get(cfg.getConnectionFactoryId()),
-            "Can not schedule message receiver, connection factory id [%s] not found. message receiver [%s].",
+            "Can not schedule message receiver task, connection factory id [%s] not found. message receiver [%s].",
             cfg.getConnectionFactoryId(), metaData);
         ses.scheduleWithFixedDelay(buildTask(metaData), cfg.getReceiveTaskInitialDelayMs(),
             cfg.getReceiveTaskDelayMs(), TimeUnit.MICROSECONDS);
+        logger.fine(() -> String.format(
+            "Scheduled message receiver task, connection factory id [%s], destination [%s], initial delay [%s]Ms",
+            metaData.getConnectionFactoryId(), metaData.getDestination(),
+            cfg.getReceiveTaskInitialDelayMs()));
       }
     }
+    anycasts.clear();
   }
 
   @PostConstruct

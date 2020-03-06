4f55df6b1e342ce0c66263417fcbbb8831c583de
bingo.chen <finesoft@gmail.com>
2020-03-06 19:06:29 +0800

    No message

--- corant-suites/corant-suites-query/corant-suites-query-shared/src/main/java/org/corant/suites/query/shared/declarative/DeclarativeQueryService.java
+++ corant-suites/corant-suites-query/corant-suites-query-shared/src/main/java/org/corant/suites/query/shared/declarative/DeclarativeQueryService.java
@@ -15,6 +15,7 @@ package org.corant.suites.query.shared.declarative;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static org.corant.shared.util.StringUtils.EMPTY;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Inherited;
 import java.lang.annotation.Retention;
@@ -25,6 +26,28 @@ import javax.enterprise.util.AnnotationLiteral;
 import org.corant.suites.query.shared.mapping.Query.QueryType;
 
 /**
+ * The declarative query service interface use for a particular query service interface. An
+ * interface specifying this annotation represents a particular query service, the query processor
+ * will generate an application scope bean object with @AutoCreated qualifier implements the
+ * interface.
+ *
+ * <pre>
+ * NOTE:
+ *
+ * 1. The interface method with default static modifier will not be dynamic implemented.
+ *
+ * 2. If use JVAVA8 then default method will avaliable, this may be changed in the future.
+ *
+ * 3. For now If the method without default and static modifier and has more than one parameter,
+ * the second parameter will not be use, we only use first parameter as query parameter,
+ * this may be changed in the future.
+ *
+ * 4. The method return type must be same as the query result type.
+ *
+ * 5. Dynamically generated methods support facilities such as CDI interceptors
+ *
+ * </pre>
+ *
  * @author bingo 下午6:30:18
  *
  */
@@ -36,8 +59,36 @@ import org.corant.suites.query.shared.mapping.Query.QueryType;
 @Stereotype
 public @interface DeclarativeQueryService {
 
-  String qualifier();
+  /**
+   * The query qualifier, typically represents a data source.
+   *
+   * <pre>
+   *
+   *  1. In SQL query <i>({@code type()} is QueryType.SQL)</i>, the qualifier represents the
+   *  data source name and the database dialect and use ':' to concat.
+   *  example: blog:MYSQL, blog is the data source name, MYSQL is the dialect.
+   *
+   *  2. In Mongodb <i>(query {@code type()})</i> is QueryType.MG, the qualifier represents the
+   *  mongodb data base name.
+   *
+   *  3. In elastic and cassandra query <i>({@code type()} is QueryType.ES or QueryType.CAS) </i>,
+   *  the qualifier represents the cluster name.
+   *
+   *  Default is empty string, meaning that if there is only one data source for the particular
+   *  query type ({@code type()}) in the application, then the qualifier represents that data source
+   *   , here data source means sql data source or mongodb database or elastic and cassandra cluster.
+   *
+   * </pre>
+   *
+   * @return qualifier
+   */
+  String qualifier() default EMPTY;
 
+  /**
+   * The query type
+   * 
+   * @return type
+   */
   QueryType type() default QueryType.SQL;
 
   public static class DeclarativeQueryServiceLiteral

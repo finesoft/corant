4618eff63037ef971ce118997e43e9baa52ca5d7
bingo.chen <finesoft@gmail.com>
2020-03-10 23:42:36 +0800

    No message

--- corant-shared/src/main/java/org/corant/shared/util/MapUtils.java
+++ corant-shared/src/main/java/org/corant/shared/util/MapUtils.java
@@ -14,7 +14,6 @@
 package org.corant.shared.util;
 
 import static org.corant.shared.util.Assertions.shouldNotNull;
-import static org.corant.shared.util.Empties.isEmpty;
 import static org.corant.shared.util.Empties.isEmptyOrNull;
 import static org.corant.shared.util.ObjectUtils.asString;
 import static org.corant.shared.util.ObjectUtils.defaultObject;
@@ -108,31 +107,6 @@ public class MapUtils {
     Map<String, String> stringMap = new HashMap<>(stringKeyMap.size());
     stringKeyMap.forEach((k, v) -> stringMap.put(k, asString(v, null)));
     return stringMap;
-  }
-
-  public static Object getKeyPathMapValue(Map<String, Object> map, String key, String splitor) {
-    if (isEmpty(map)) {
-      return null;
-    }
-    String[] keys = split(key, splitor, true, false);
-    if (keys.length == 0) {
-      return null;
-    } else if (keys.length == 1) {
-      return map.get(keys[0]);
-    } else {
-      int len = keys.length - 1;
-      String matchedKey = keys[len];
-      Map<String, Object> useMap = map;
-      for (int i = 0; i < len; i++) {
-        if ((useMap = getMapMap(useMap, keys[i])) == null) {
-          break;
-        }
-      }
-      if (useMap != null) {
-        return useMap.get(matchedKey);
-      }
-      return null;
-    }
   }
 
   public static BigDecimal getMapBigDecimal(final Map<?, ?> map, final Object key) {
@@ -471,30 +445,6 @@ public class MapUtils {
     Properties result = new Properties();
     mapOf((Object[]) strings).forEach(result::put);
     return result;
-  }
-
-  public static void replaceKeyPathMapValue(Map<String, Object> map, String key, String keySplitor,
-      Function<Object, Object> func) {
-    replaceKeyPathMapValue(map, split(key, keySplitor, true, false), func);
-
-  }
-
-  public static void replaceKeyPathMapValue(Map<String, Object> map, String[] keyPath,
-      Function<Object, Object> func) {
-    if (map == null || isEmpty(keyPath)) {
-      return;
-    }
-    Map<String, Object> useMap = map;
-    int len = keyPath.length - 1;
-    String putKey = keyPath[len];
-    for (int i = 0; i < len; i++) {
-      if ((useMap = getMapMap(useMap, keyPath[i])) == null) {
-        break;
-      }
-    }
-    if (useMap != null) {
-      useMap.put(putKey, func.apply(useMap.get(putKey)));
-    }
   }
 
   public static Map<String, String> toMap(final Properties properties) {

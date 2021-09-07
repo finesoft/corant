2a2b56bc357d3f88ef8a1c885c69ec26cdc97b16
bingo <finesoft@gmail.com>
2021-09-07 09:59:07 +0800

    No message

--- corant-context/src/main/java/org/corant/context/concurrent/ManagedExecutorConfig.java
+++ corant-context/src/main/java/org/corant/context/concurrent/ManagedExecutorConfig.java
@@ -135,8 +135,8 @@ public class ManagedExecutorConfig extends AbstractNamedObject implements Declar
 
   @Override
   public String toString() {
-    return "[longRunningTasks=" + longRunningTasks + "ms, hungTaskThreshold=" + hungTaskThreshold
-        + ", corePoolSize=" + corePoolSize + ", maxPoolSize=" + maxPoolSize + ", keepAliveTime="
+    return "[longRunningTasks=" + longRunningTasks + ", hungTaskThreshold=" + hungTaskThreshold
+        + "ms, corePoolSize=" + corePoolSize + ", maxPoolSize=" + maxPoolSize + ", keepAliveTime="
         + keepAliveTime + ", threadLifeTime=" + threadLifeTime + ", awaitTermination="
         + awaitTermination + ", rejectPolicy=" + rejectPolicy + ", threadPriority=" + threadPriority
         + ", threadName=" + threadName + ", queueCapacity=" + queueCapacity + ", contextInfos="

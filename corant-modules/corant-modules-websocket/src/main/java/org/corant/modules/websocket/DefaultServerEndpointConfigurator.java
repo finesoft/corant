dc317b7184bcff1045e0c4d4d66e94665ed0ef0f
bingo <finesoft@gmail.com>
2022-04-29 22:18:46 +0800

    No message

---
+++ corant-modules/corant-modules-websocket/src/main/java/org/corant/modules/websocket/DefaultServerEndpointConfigurator.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013-2021, Bingo.Chen (finesoft@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.corant.modules.websocket;
+
+import static org.corant.context.Beans.resolve;
+import javax.websocket.server.ServerEndpointConfig.Configurator;
+
+/**
+ * corant-modules-websocket
+ *
+ * @author bingo 下午10:10:38
+ *
+ */
+public class DefaultServerEndpointConfigurator extends Configurator {
+
+  @Override
+  public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
+    return resolve(endpointClass);
+  }
+
+}

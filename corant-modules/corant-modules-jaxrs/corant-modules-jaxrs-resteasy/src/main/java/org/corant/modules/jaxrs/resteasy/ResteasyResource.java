ea9a6382d6fdd55da60d1691a1b2d18b539babaa
bingo <finesoft@gmail.com>
2022-05-08 21:27:20 +0800

    No message

--- corant-modules/corant-modules-jaxrs/corant-modules-jaxrs-resteasy/src/main/java/org/corant/modules/jaxrs/resteasy/ResteasyResource.java
+++ corant-modules/corant-modules-jaxrs/corant-modules-jaxrs-resteasy/src/main/java/org/corant/modules/jaxrs/resteasy/ResteasyResource.java
@@ -15,10 +15,14 @@ package org.corant.modules.jaxrs.resteasy;
 
 import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
 import static javax.ws.rs.core.HttpHeaders.CONTENT_DISPOSITION;
 import static org.corant.modules.servlet.ContentDispositions.parse;
 import static org.corant.shared.util.Assertions.shouldNotNull;
 import static org.corant.shared.util.Empties.isEmpty;
+import static org.corant.shared.util.Empties.isNotEmpty;
+import static org.corant.shared.util.Empties.sizeOf;
 import static org.corant.shared.util.Objects.defaultObject;
 import static org.corant.shared.util.Strings.isNotBlank;
 import java.io.IOException;
@@ -29,13 +33,18 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import javax.ws.rs.core.GenericType;
 import org.apache.james.mime4j.codec.DecodeMonitor;
 import org.apache.james.mime4j.codec.DecoderUtil;
 import org.corant.modules.jaxrs.shared.AbstractJaxrsResource;
 import org.corant.modules.servlet.ContentDispositions.ContentDisposition;
+import org.corant.shared.exception.CorantRuntimeException;
 import org.corant.shared.resource.Resource;
 import org.corant.shared.resource.SourceType;
+import org.corant.shared.ubiquity.Tuple.Pair;
 import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 /**
  * corant-modules-jaxrs-resteasy
@@ -181,6 +190,188 @@ public class ResteasyResource extends AbstractJaxrsResource {
         return cls.cast(this);
       }
       return Resource.super.unwrap(cls);
+    }
+
+  }
+
+  /**
+   * corant-modules-jaxrs-resteasy
+   *
+   * @author bingo 上午12:42:18
+   *
+   */
+  public static class MultipartFormDataExtractor implements AutoCloseable {
+
+    final MultipartFormDataInput formData;
+    final Map<String, List<InputPart>> formDataMap;
+
+    public MultipartFormDataExtractor(MultipartFormDataInput formData) {
+      this.formData = shouldNotNull(formData);
+      formDataMap = formData.getFormDataMap();
+    }
+
+    @Override
+    public void close() throws Exception {
+      formData.close();
+    }
+
+    /**
+     * Returns and converts the single filed value by given class.
+     *
+     * @param <T> the expected field type
+     * @param fieldName the field name to get
+     * @param clazz the expected field class
+     * @return the field value or null, if the field is not found or the field value is found to be
+     *         null
+     */
+    public <T> T getField(String fieldName, Class<T> clazz) {
+      List<InputPart> ips = formDataMap.get(fieldName);
+      InputPart part;
+      if (isNotEmpty(ips) && (part = ips.get(0)) != null) {
+        try {
+          return part.getBody(clazz, null);
+        } catch (IOException ex) {
+          throw new CorantRuntimeException(ex);
+        }
+      }
+      return null;
+    }
+
+    /**
+     * Returns and converts the single filed value by given class.
+     *
+     * @see GenericType
+     *
+     * @param <T> the expected field type
+     * @param fieldName the field name to get
+     * @param type the expected field generic type
+     * @return the field value or null, if the field is not found or the field value is found to be
+     *         null
+     */
+    public <T> T getField(String fieldName, GenericType<T> type) {
+      List<InputPart> ips = formDataMap.get(fieldName);
+      InputPart part;
+      if (isNotEmpty(ips) && (part = ips.get(0)) != null) {
+        try {
+          return part.getBody(type);
+        } catch (IOException ex) {
+          throw new CorantRuntimeException(ex);
+        }
+      }
+      return null;
+    }
+
+    /**
+     * Returns a list of values for a set of fields with the same given name, or empty list if the
+     * field not found. Note: the returns field value list is unmodifiable.
+     *
+     * @param <T>
+     * @param fieldName
+     * @param clazz
+     * @return getFields
+     */
+    public <T> List<T> getFields(String fieldName, Class<T> clazz) {
+      List<InputPart> ips = formDataMap.get(fieldName);
+      if (isNotEmpty(ips)) {
+        List<T> list = new ArrayList<>();
+        for (InputPart ip : ips) {
+          if (ip != null) {
+            try {
+              list.add(ip.getBody(clazz, null));
+            } catch (IOException e) {
+              throw new CorantRuntimeException(e);
+            }
+          }
+        }
+        return unmodifiableList(list);
+      }
+      return emptyList();
+    }
+
+    /**
+     * Returns a matrix of all field value lists based on the given field name array, where each
+     * element (Map) in the list corresponds to the values of multiple fields, used to process
+     * tabular data forms.
+     *
+     * @param fieldNames the field names
+     * @return the value lists
+     */
+    public List<Map<String, Object>> getFieldsMatrix(String... fieldNames) {
+      if (fieldNames.length > 0) {
+        Map<String, List<Object>> temp = new LinkedHashMap<>(fieldNames.length);
+        List<Map<String, Object>> result = new ArrayList<>();
+        for (String fieldName : fieldNames) {
+          List<InputPart> ips = formDataMap.get(fieldName);
+          int valueSize = sizeOf(ips);
+          int resultSize = result.size();
+          if (valueSize > 0) {
+            for (int i = 0; i < valueSize - resultSize; i++) {
+              result.add(new LinkedHashMap<>());
+            }
+            List<Object> fieldValues = new ArrayList<>(valueSize);
+            for (InputPart ip : ips) {
+              try {
+                ContentDisposition cd = parse(ip.getHeaders().getFirst(CONTENT_DISPOSITION));
+                if (cd.getFilename() != null) {
+                  fieldValues.add(new InputPartResource(ip, cd));
+                } else {
+                  fieldValues.add(ip.getBody(String.class, null));
+                }
+              } catch (IOException e) {
+                throw new CorantRuntimeException(e);
+              }
+            }
+            temp.put(fieldName, fieldValues);
+          }
+        }
+        temp.forEach((fn, fvs) -> {
+          int fs = fvs.size();
+          for (int i = 0; i < fs; i++) {
+            result.get(i).put(fn, fvs.get(i));
+          }
+        });
+        return unmodifiableList(result);
+      }
+      return emptyList();
+    }
+
+    /**
+     * Returns an uploaded file resource based on the given field name, or throws an exception if
+     * the specified field is not a file field.
+     *
+     * @param fieldName the field name
+     * @return the uploaded file.
+     */
+    public InputPartResource getFile(String fieldName) {
+      List<InputPart> ips = formDataMap.get(fieldName);
+      if (isNotEmpty(ips)) {
+        return ips.stream()
+            .map(ip -> Pair.of(parse(ip.getHeaders().getFirst(CONTENT_DISPOSITION)), ip))
+            .filter(p -> p.left().getFilename() != null)
+            .map(p -> new InputPartResource(p.right(), p.left())).findFirst().orElseThrow(
+                () -> new CorantRuntimeException("The field name %s is not a file", fieldName));
+      }
+      return null;
+    }
+
+    /**
+     * Returns a list of uploaded file resources with the same name according to the given field
+     * name. If the specified field is not a file or the field value is empty, an empty list is
+     * returned. Note: the returned list is unmodifiable.
+     *
+     * @param fieldName
+     * @return getFiles
+     */
+    public List<InputPartResource> getFiles(String fieldName) {
+      List<InputPart> ips = formDataMap.get(fieldName);
+      if (isNotEmpty(ips)) {
+        return ips.stream()
+            .map(ip -> Pair.of(parse(ip.getHeaders().getFirst(CONTENT_DISPOSITION)), ip))
+            .filter(p -> p.left().getFilename() != null)
+            .map(p -> new InputPartResource(p.right(), p.left()))
+            .collect(Collectors.toUnmodifiableList());
+      }
+      return emptyList();
     }
 
   }

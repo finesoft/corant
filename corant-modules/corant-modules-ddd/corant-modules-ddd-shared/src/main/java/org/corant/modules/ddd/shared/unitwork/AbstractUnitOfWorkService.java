c03d87ab78492523112b2e527e685738cbb45c61
bingo <finesoft@gmail.com>
2023-01-03 16:10:42 +0800

    No message

---
+++ corant-modules/corant-modules-ddd/corant-modules-ddd-shared/src/main/java/org/corant/modules/ddd/shared/unitwork/AbstractUnitOfWorkService.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2013-2021, Bingo.Chen (finesoft@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.corant.modules.ddd.shared.unitwork;
+
+import java.io.Serializable;
+import javax.annotation.PreDestroy;
+import javax.transaction.Status;
+import javax.transaction.SystemException;
+import javax.transaction.TransactionScoped;
+import org.corant.modules.jta.shared.TransactionService;
+import org.corant.shared.exception.CorantRuntimeException;
+
+/**
+ * corant-modules-ddd-shared
+ * <p>
+ * A CDI service abstract class with UnitOfWork (transaction) as the bean scope, which supports the
+ * callback {@link #onComplete(boolean)} operation of the UnitOfWork.
+ * <p>
+ * This class has many uses, such as tracking entity state changes, encapsulating the changed state
+ * as an event, and emitting the event when UnitOfWork is successfully completed.
+ * <p>
+ * Examples:
+ *
+ * <pre>
+ * public class StateTracer extends AbstractUnitOfWorkService {
+ *
+ *   final Set<Serializable> states = new LinkedHashSet<>();
+ *
+ *   public void registerState(Serializable state) {
+ *     this.states.add(state);
+ *   }
+ *
+ *   protected void onComplete(boolean success) {
+ *     if (success) {
+ *       CDIs.fireEvent(new StateEvent(states));
+ *     }
+ *   }
+ *
+ * }
+ * </pre>
+ *
+ *
+ * @author bingo 下午12:23:20
+ */
+@TransactionScoped
+public abstract class AbstractUnitOfWorkService implements Serializable {
+
+  private static final long serialVersionUID = -4395687658846903572L;
+
+  protected void onComplete(boolean success) {}
+
+  /**
+   * Called by {@link javax.transaction.Synchronization#afterCompletion(int)}
+   */
+  @PreDestroy
+  protected void onPreDestroy() {
+    try {
+      final int status = TransactionService.currentTransaction().getStatus();
+      onComplete((status == Status.STATUS_COMMITTED));
+    } catch (SystemException e) {
+      throw new CorantRuntimeException(e);
+    }
+  }
+}

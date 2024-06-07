b9a313d196588ddb0f0e8100c79ddbed803529ac
bingo <finesoft@gmail.com>
2024-06-07 15:41:23 +0800

    No message

--- corant-modules/corant-modules-vertx/corant-modules-vertx-shared/src/main/java/org/corant/modules/vertx/shared/Verticles.java
+++ corant-modules/corant-modules-vertx/corant-modules-vertx-shared/src/main/java/org/corant/modules/vertx/shared/Verticles.java
@@ -13,9 +13,16 @@
  */
 package org.corant.modules.vertx.shared;
 
+import static org.corant.shared.util.Assertions.shouldNotNull;
+import static org.corant.shared.util.Objects.defaultObject;
+import java.time.Duration;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
 import org.corant.Corant;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 
 /**
@@ -24,6 +31,28 @@ import io.vertx.core.Vertx;
  * @author bingo 下午3:46:50
  */
 public class Verticles {
+
+  public static <T> T getFutureResult(Future<T> future)
+      throws InterruptedException, ExecutionException {
+    shouldNotNull(future, "Future can't null");
+    return future.toCompletionStage().toCompletableFuture().get();
+  }
+
+  public static <T> T getFutureResult(Future<T> future, Duration timeout)
+      throws InterruptedException, ExecutionException, TimeoutException {
+    if (timeout == null || timeout.toMillis() <= 0) {
+      return getFutureResult(future);
+    }
+    return future.toCompletionStage().toCompletableFuture().get(timeout.toMillis(),
+        TimeUnit.MILLISECONDS);
+  }
+
+  public static <T> T getFutureResult(Future<T> future, long timeout, TimeUnit timeUnit)
+      throws InterruptedException, ExecutionException, TimeoutException {
+    shouldNotNull(future, "Future can't null");
+    return future.toCompletionStage().toCompletableFuture().get(timeout,
+        defaultObject(TimeUnit.MILLISECONDS, timeUnit));
+  }
 
   public static void single(Consumer<SeContainerInitializer> initializer, String... arguments) {
     Vertx.vertx().deployVerticle(new CorantVerticle(initializer, arguments));
